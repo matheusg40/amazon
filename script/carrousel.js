0ace29be2c12a6b747d174259431104817ba66d0
matheusg40 <51178161+matheusg40@users.noreply.github.com>
2021-10-28 17:29:25 -0300

    2.0

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,6 +1,5 @@
-
 var containerImgs = document.querySelector(".vitrine-galeria");
-let imgs = document.querySelectorAll('.vitrine-galeria img');
+var imgs = document.querySelectorAll('.vitrine-galeria img');
 let initialScroll = 0;
 
 let targetWidth = (containerImgs.clientWidth * imgs.length) - containerImgs.clientWidth;
@@ -8,40 +7,49 @@ let targetWidth = (containerImgs.clientWidth * imgs.length) - containerImgs.clie
 let scrollSpd = 10;
 let vitrineGaleriaWraper = document.querySelector('.vitrine-galeria-wraper');
 
- 
+
 //onde a gente quer chega menos o atual e fazer com que pare em cada um
 
 
 
-vitrineGaleriaWraper.style.width=(imgs.length * 100) + '%';
-    for(let i = 0; i<imgs.length; i++){
-            imgs[i].style.width = (100/imgs.length)+'%';
-        
-    }
+vitrineGaleriaWraper.style.width = (imgs.length * 100) + '%';
+for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.width = (100 / imgs.length) + '%';
 
-    let startTime = 3;
+}
 
-        startCarrousel();
+let startTime = 3;
+var curSlider = 1; //imagem atual
 
-    function startCarrousel(){
-                setTimeout(function(){
-                        runCarrousel();
-                }, startTime*1000);
-        }
-        function runCarrousel(){
-                var interval = setInterval(function(){
-                        initialScroll-=scrollSpd;
-                        for(let i = 0; i<imgs.length; i++){
-                                imgs[i].style.left = initialScroll+"px";
-                            
-                        }
-                        if(initialScroll <= -targetWidth){
-                        
-                                                      
+
+
+
+startCarrousel();
+
+function startCarrousel() {
+        setTimeout(function () {
+                runCarrousel();
+        }, startTime * 1000);
+}
+
+function runCarrousel() {
+        var interval = setInterval(function () {
+                initialScroll -= scrollSpd;
+                for (let i = 0; i < imgs.length; i++) {
+                        imgs[i].style.left = initialScroll + "px";
+
+                }
+                if (initialScroll <= -containerImgs.clientWidth * curSlider) {
+
+                        curSlider++;
+                        if (curSlider == imgs.length) {
+                                clearInterval(interval);
+                        } else {
+
                                 clearInterval(interval);
                                 startCarrousel();
                         }
+                }
 
-                },0.5);
-        }
-        
+        }, 0.5);
+}
