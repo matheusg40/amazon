b031a4723a517d2f403986171421d38ff36d1b00
matheusg40 <51178161+matheusg40@users.noreply.github.com>
2021-10-28 12:11:30 -0300

    js
    atualização do javascript

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,4 +1,5 @@
-let containerImgs = document.querySelector*(".vitrine-galeria");
+
+let containerImgs = document.querySelector(".vitrine-galeria");
 let imgs = document.querySelectorAll('.vitrine-galeria img');
 let initialScroll = 0;
 
@@ -7,11 +8,30 @@ let targetWidth = (containerImgs.clientWidth * imgs.length) - containerImgs.clie
 let scrollSpd = 10;
 let vitrineGaleriaWraper = document.querySelector('.vitrine-galeria-wraper');
 
+ 
 vitrineGaleriaWraper.style.width=(imgs.length * 100) + '%';
-
     for(let i = 0; i<imgs.length; i++){
             imgs[i].style.width = (100/imgs.length)+'%';
         
     }
-    
 
+    let startTime = 5;
+
+        setTimeout(function(){
+                startCarrousel();
+        }, startTime*1000);
+
+        function startCarrousel(){
+                var interval = setInterval(function(){
+                        initialScroll-=scrollSpd;
+                        for(let i = 0; i<imgs.length; i++){
+                                imgs[i].style.left = initialScroll+"px";
+                            
+                        }
+                        if(initialScroll <= -targetWidth){
+                                clearInterval(interval);
+                        }
+
+                },0.5);
+        }
+        
